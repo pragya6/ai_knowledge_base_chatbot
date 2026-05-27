ce2b942c5bff76fc8ebd577bed330b58278e4e4e
Pragya-29kreativ <pragyasinghal@29kreativ.com>
2026-05-28 00:36:45 +0530

    refactor(Streamlit): uncommented code for Streamlit Secrets

--- faiss_app.py
+++ faiss_app.py
@@ -16,9 +16,9 @@ from langchain_community.vectorstores import FAISS
 load_dotenv() # local path to api key
 
 # For Streamlit Cloud
-# if "GOOGLE_API_KEY" not in os.environ:
-#     if "GOOGLE_API_KEY" in st.secrets:
-#         os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"] 
+if "GOOGLE_API_KEY" not in os.environ:
+    if "GOOGLE_API_KEY" in st.secrets:
+        os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"] 
 
 # Setup logger
 logging.basicConfig(level=logging.WARNING, filename="errors.log")
